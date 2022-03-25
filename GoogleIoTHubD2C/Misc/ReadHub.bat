287fd2a5d5e93548fce2d34cfa832461f7df8ad3
David Jones <davidjones@sportronics.com.au>
2022-03-25 17:28:29 +1100

    Do.bat renamed. Also use .ps1 for env vars

---
+++ GoogleIoTHubD2C/Misc/ReadHub.bat
@@ -0,0 +1,3 @@
+call ./setenv.ps1
+<Path to .exe>\GoogleIoTHubD2C.exe
+cast  -device "Nest"  -file c:\temp\temperature.txt
--- GoogleIoTHubD2C/Misc/do.bat
+++
@@ -1,3 +0,0 @@
-call setenv.bat
-dotnet run
-cast  -device "Nest"  -file c:\temp\temperature.txt
