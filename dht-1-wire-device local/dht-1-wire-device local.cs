794a44640d4d645389e2c9653ac61818b662eac5
David Jones <davidjones@sportronics.com.au>
2022-03-28 16:34:17 +1100

    Added dht22 1 wire device console app local

---
+++ dht-1-wire-device local/dht-1-wire-device local.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// This application uses the Azure IoT Hub device SDK for .NET
+// For samples see: https://github.com/Azure/azure-iot-sdk-csharp/tree/master/iothub/device/samples
+
+using System;
+using Microsoft.Azure.Devices.Client;
+using System.Threading.Tasks;
+using SendTelemetry;
+
+namespace simulated_device
+{
+    class SimulatedDeviceLocal
+    {
+
+        // The device connection string to authenticate the device with your IoT hub.
+        // Using the Azure CLI:
+        // az iot hub device-identity connection-string show --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
+        ////private readonly static string s_connectionString = "{Your device connection string here}";
+        
+        // For this sample either
+        // - pass this value as a command-prompt argument
+        // - set the IOTHUB_DEVICE_CONN_STRING environment variable 
+        // - create a launchSettings.json (see launchSettings.json.template) containing the variable
+        private static string s_connectionString = Environment.GetEnvironmentVariable("IOTHUB_DEVICE_CONN_STRING");
+
+        private static int period;
+
+
+
+        // Async method to send simulated telemetry
+        private static async void SendDeviceToCloudMessagesAsync(int maxReties, int dht22Gpio)
+        {
+
+            while (true)
+            {
+
+                // Create JSON message
+                var telemetryDataPoint = dht1wiredevicelocal.dht221wire.GetTempDHTxx1Wire(maxReties, dht22Gpio);
+
+                if (telemetryDataPoint != null)
+                {
+                    // Send the telemetry message
+                    await DeviceSendTelemetryToHub.SendDeviceToCloudMessageAsync(telemetryDataPoint, s_connectionString);
+                }
+
+                await Task.Delay(period*1000);
+            }
+        }
+        private static void Main(string[] args)
+        {
+            period = 10; //sec
+            int maxReties = 10;
+            // Set this to the RPi GPIO connection for DHT22 1-Wire
+            // eg https://github.com/djaus2/DNETCoreGPIO/blob/master/DNETCoreGPIO/Circuits/dht22.png  .. Its 26 there
+            int dht22Gpio = 25;
+            Console.WriteLine("IoT Hub Quickstarts #1 - Simulated device. Ctrl-C to exit.\n");
+
+            if (args.Length > 0)
+            {
+                if (int.TryParse(args[0], out int iperiod))
+                {
+                    period = iperiod;
+                }
+                if (args.Length > 1)
+                    if (args[1].Length > 20)
+                        s_connectionString = args[1];
+            }
+
+            Console.WriteLine("Using Env Var IOTHUB_DEVICE_CONN_STRING = " + s_connectionString);
+
+            SendDeviceToCloudMessagesAsync(maxReties, dht22Gpio);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dht-1-wire-device local/dht22-1-wire.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Device.Gpio;
+using Iot.Device;
+using Iot.Device.CpuTemperature;
+using System.Device.I2c;
+using Iot.Device.Bmp180;
+using Iot.Device.DHTxx;
+
+namespace dht1wiredevicelocal
+{ 
+    public static class dht221wire
+    {
+        public static dynamic GetTempDHTxx1Wire(int maxNumTries, int dht22GPio=25)
+        {
+            int numTries = 0;
+            dynamic datapoint = null;
+            //1-Wire:
+
+            Console.WriteLine("Using DH22-1-Wire1");
+            bool lastResult = true;
+            using (Dht22 dht = new Dht22(dht22GPio))
+            {
+                if (dht == null)
+                {
+                    Console.WriteLine("Dht22 instantiation failed");
+                    return datapoint;
+
+                }
+                else
+                {
+                    while (true)
+                    {
+                        UnitsNet.Temperature temp;
+                        UnitsNet.RelativeHumidity humid;
+                        bool result1 = dht.TryReadTemperature(out temp);
+                        bool result2 = dht.TryReadHumidity(out humid);
+                        if (!result1 || !result2)
+                        {
+                            Console.Write(".");
+                            lastResult = false;
+                            if (++numTries >= maxNumTries)
+                                return datapoint;
+                        }
+                        else
+                        {
+                            //Sanity Check
+                            bool resultIsValid = true;
+                            if (temp.DegreesCelsius is double.NaN)
+                                resultIsValid = false;
+                            else if (humid.Percent is double.NaN)
+                                resultIsValid = false;
+                            if (!resultIsValid)
+                            {
+                                Console.Write("#");
+                                lastResult = false;
+                                if (++numTries >= maxNumTries)
+                                    return datapoint;
+                            }
+                            else
+                            {
+                                bool resultIsSane = true;
+                                if ((temp.DegreesCelsius > 100) || (temp.DegreesCelsius < -20))
+                                    resultIsSane = false;
+                                else if ((humid.Percent > 100) || (humid.Percent < 0))
+                                    resultIsSane = false;
+                                if (!resultIsSane)
+                                {
+                                    Console.Write("x");
+                                    lastResult = false;
+                                    if (++numTries >= maxNumTries)
+                                        return datapoint;
+                                }
+                                else
+                                {
+
+                                    if (!lastResult)
+                                    {
+                                        Console.WriteLine("");
+                                    }
+                                    lastResult = true;
+                                    Console.Write($"Temperature: {temp.DegreesCelsius.ToString("0.0")} °C ");
+                                    Console.Write($"Humidity: { humid.Percent.ToString("0.0")} % ");
+                                    Console.WriteLine("");
+                                    string result = $"Temperature equals {temp.DegreesCelsius.ToString("0.0")} °C ,";
+                                    result += $"and Humidity equals { humid.Percent.ToString("0.0")} % ";
+
+                                    datapoint = new
+                                    {
+                                        temperature = temp.DegreesCelsius,
+                                        humidity = humid.Percent
+                                    };
+                                    return datapoint;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
