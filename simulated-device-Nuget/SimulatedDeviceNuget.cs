735642462ad0c600b68550b12075511e218310b0
David Jones <davidjones@sportronics.com.au>
2022-03-30 17:05:34 +1100

    done

--- simulated-device-Nuget/SimulatedDeviceNuget.cs
+++ simulated-device-Nuget/SimulatedDeviceNuget.cs
@@ -28,7 +28,7 @@ namespace simulated_device
 
 
         // Async method to send simulated telemetry
-        private static async void SendDeviceToCloudMessagesAsync()
+        private static async Task SendDeviceToCloudMessagesAsync()
         {
             // Initial telemetry values
             double minTemperature = 20;
@@ -53,7 +53,7 @@ namespace simulated_device
                 await Task.Delay(period*1000);
             }
         }
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             period = 10;
             Console.WriteLine("IoT Hub Quickstarts #1 - Simulated device. Ctrl-C to exit.\n");
@@ -71,8 +71,8 @@ namespace simulated_device
 
             Console.WriteLine("Using Env Var IOTHUB_DEVICE_CONN_STRING = " + s_connectionString);
 
-            SendDeviceToCloudMessagesAsync();
-            Console.ReadLine();
+            await SendDeviceToCloudMessagesAsync();
+            Console.WriteLine("Done");
         }
     }
 }
